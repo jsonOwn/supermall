98bb6661ba61e03a740071558a066c9dbb6b8892
jsonOwn <1090368437@qq.com>
2021-07-10 15:21:54 +0800

    第一次提交

---
+++ src/views/home/Home.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+    <recommend-view :recommends="recommend"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control
+      :titles="['流行', '新款', '精选']"
+      class="tab-control"
+    ></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
+  </div>
+  
+</template>
+
+<script>
+import NavBar from "components/common/NavBar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from 'components/content/goods/GoodsList'
+
+import { getHomeMultdata } from "network/home";
+import { getHomeGoods } from "network/home";
+
+import HomeSwiper from "./childerComps/homeSwiper";
+import RecommendView from "./childerComps/RecommendView";
+import FeatureView from "./childerComps/FeatureView";
+
+
+export default {
+  name: "Home",
+  components: {
+    FeatureView,
+    RecommendView,
+    NavBar,
+    HomeSwiper,
+    TabControl,
+    GoodsList
+  },
+  data() {
+    return {
+      banner: [],
+      recommend: [],
+      goods: {
+        pop: {
+          page: 0,
+          list: [],
+        },
+        new: {
+          page: 0,
+          list: [],
+        },
+        sell: {
+          page: 0,
+          list: [],
+        },
+      },
+    };
+  },
+  created() {
+    this.getHomeMultdata(),
+      this.getHomeGoods("pop"),
+      this.getHomeGoods("new"),
+      this.getHomeGoods("sell")
+  },
+  methods: {
+    getHomeMultdata() {
+      getHomeMultdata().then((res) => {
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    }
+  },
+}
+</script>
+
+<style scoped>
+#home {
+  padding-top: 44px;
+}
+
+.home-nav {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  background-color: #fff;
+}
+</style>
---
+++ src/views/home/childerComps/homeSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banner">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from 'components/common/swiper/index'
+
+export default {
+  name: "childerSwiper",
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  props: {
+    banner: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
